3a85b491f3c8febed1b89eae23095d54a860d384
Jeremy Hansen <jhansen1344@gmail.com>
2020-05-09 14:20:23 -0400

    user and repository added

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -1,4 +1,5 @@
 using DatingApp.API.Controllers.Models;
+using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers.Data
@@ -10,5 +11,6 @@ namespace DatingApp.API.Controllers.Data
         }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users{get;set;}
     }
 }
