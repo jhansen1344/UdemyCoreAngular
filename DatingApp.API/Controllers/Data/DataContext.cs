e8878e16c3a4b4ce10ccb7523adf767c568acd32
Jeremy Hansen <jhansen1344@gmail.com>
2020-05-09 12:10:00 -0400

    InitialCommit

---
+++ DatingApp.API/Controllers/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DatingApp.API.Controllers.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
